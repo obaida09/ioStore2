6219ee79f9b304fbef21f08a781828c97f4d81da
Obaida Mohammed <obeda.aljorany@gmail.com>
2022-06-19 14:33:33 +0300

    dd

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -19,7 +19,7 @@
       <h2 class="h5 text-uppercase mb-4">Browse our categories</h2>
     </header>
     <div class="row">
-      <div class="col-md-4"><a class="category-item" href="shop.html"><img class="img-fluid" src="{{ asset('frontend/img/cat-img-1.jpg') }}" alt="" /><strong class="category-item-title">Clothes</strong></a>
+      <div class="col-md-4"><a class="category-item asfa" href="{{ route('frontend.product', $product_categories[0]->slug) }}"><img class="img-fluid" src="{{ asset('frontend/img/cat-img-1.jpg') }}" alt="" /><strong class="category-item-title">Clothes</strong></a>
       </div>
       <div class="col-md-4"><a class="category-item mb-4" href="shop.html"><img class="img-fluid" src="{{ asset('frontend/img/cat-img-2.jpg') }}" alt="" /><strong class="category-item-title">Shoes</strong></a><a class="category-item" href="shop.html"><img class="img-fluid" src="{{ asset('frontend/img/cat-img-3.jpg') }}" alt="" /><strong class="category-item-title">Watches</strong></a>
       </div>
